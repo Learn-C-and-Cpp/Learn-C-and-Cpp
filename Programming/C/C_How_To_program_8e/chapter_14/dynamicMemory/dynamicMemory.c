9cad6dd283ae31eeed47865e1b02c412ffbc9fa9
Keino.Eliud <elkeino@gmail.com>
2022-07-05 06:01:53 +0300

    added dymicMemory and goto

---
+++ Programming/C/C_How_To_program_8e/chapter_14/dynamicMemory/dynamicMemory.c
@@ -0,0 +1,14 @@
+/* Dynamic memory allocation: functions calloc and realloc in stdlib.h
+    
+    void *calloc(size_t nmemb, size_t size);
+    Note// arguments are: number of elements and the size or each element size. 
+
+    calloc - clears the memory is allocates 
+    malloc - dose not clears the memory is allocates
+    realloc - changes the  size of an object allocated gy provious call to mallo,
+    calloc and realloc.
+
+    void *realloc(void *ptr, size_t size);
+    Note// arguments are: pointer to the original object and the new size of object.
+*/
+
