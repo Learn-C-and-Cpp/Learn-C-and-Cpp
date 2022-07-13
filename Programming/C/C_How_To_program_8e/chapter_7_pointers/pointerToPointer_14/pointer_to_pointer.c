673073a0d1e101eb99e9871562f985a9539100fa
Keino.Eliud <elkeino@gmail.com>
2022-07-13 08:48:58 +0300

    modified pointer to pointer

--- Programming/C/C_How_To_program_8e/chapter_7_pointers/pointerToPointer_14/pointer_to_pointer.c
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointerToPointer_14/pointer_to_pointer.c
@@ -8,6 +8,19 @@ int main(void)
     char* str = "This is a test";
     printOutput(&str);
     printf(" After the call: %s\n", str);
+
+    int x = 5;
+    int *p = &x;
+    *p = 6;
+    int **q = &p;
+    int *** r = &q;
+
+     printf("x = %d\n", *p);
+     printf("*p = %d\n", *q);
+     printf("**q = %d\n", **q);
+     printf("**r = %d\n", **r);
+     printf("***r = %d\n", ***r);
+     ***r = 10;
     
     return 0;
 }
