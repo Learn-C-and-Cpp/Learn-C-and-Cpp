124267828a39b3c537a56b7aeba9d4989f15bb12
Keino Eliud <elkeino@gmail.com>
2022-07-15 05:52:20 +0300

    array and pointer

--- Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_freeCode/charArrayAndPointers/charArrayAndPointers.c
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_freeCode/charArrayAndPointers/charArrayAndPointers.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void printStr(char* C)
+void printStr(const char* C)
 {
+    //C[0] = 'A';
     while (*C != '\0')
     {
         printf("%c ", *C);
@@ -13,6 +14,7 @@ void printStr(char* C)
 int main(void)
 {
     char C[20] = "Hello"; //
+   // char *C = "Hello"; // C becomes a constant cannot be changed
     printStr(C);
 
     return 0;
