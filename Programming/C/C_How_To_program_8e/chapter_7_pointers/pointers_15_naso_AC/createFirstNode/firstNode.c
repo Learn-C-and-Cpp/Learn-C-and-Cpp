ebb63087c33f64a4ab9735af8270e27a7102ebe9
Keino Eliud <elkeino@gmail.com>
2022-08-17 04:45:26 +0300

    creating node

--- Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_naso_AC/createFirstNode/firstNode.c
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_naso_AC/createFirstNode/firstNode.c
@@ -1,4 +1,4 @@
-// creating the first node of the list
+// creating a linked list
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,25 +6,71 @@
 struct node
 {
     int data;
-    struct  node* link;
+    struct node *link;
 };
 typedef struct node Node_t;
+
+// prototypes
+void travers_counting_nodes(Node_t *head);
+void print_data(Node_t* head);
 
 int main()
 {
     Node_t *head = NULL;
     Node_t *current = NULL;
-
-    head = (Node_t*)malloc(sizeof(Node_t)); // allocate memory for this node
-    head->data = 45; // set data
-    head->link = NULL;
-    printf("%d ", head->data);
     
-    current = (Node_t*)malloc(sizeof(Node_t)); // allocate memory for this node
-    current->data = 98; // set data
+    // 1st node
+    head = (Node_t *)malloc(sizeof(Node_t)); // allocate memory
+    head->data = 45;
+    head->link = NULL;
+    // 2nd node
+    current = (Node_t *)malloc(sizeof(Node_t)); // allocate memory
+    current->data = 98;
     current->link = NULL;
-    printf("%d ", current->data);
+    head->link = current;
 
+    // 2nd node
+    current = (Node_t *)malloc(sizeof(Node_t)); // allocate memory
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
+    // printf("%d", head->link->link->data);
+    //  counting of nodes
+    travers_counting_nodes(head);
+    print_data(head);
 
     return 0;
+}
+// counting list nodes
+void travers_counting_nodes(Node_t *head)
+{
+    int count = 0;
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+    }
+
+    Node_t *ptr = NULL;// new pointer to travers the list
+    ptr = head; // ptr takes the value of head
+
+    while (ptr != NULL) // loop untill ptr is NULL
+    {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("count = %d\n", count);
+}
+
+// print data
+void print_data(Node_t* head)
+{
+    Node_t *ptr = NULL;// new pointer to travers the list
+    ptr = head; // ptr takes the value of head
+
+    while (ptr != NULL) // loop untill ptr is NULL
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
 }
