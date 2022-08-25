517736ca765b4574d47e811258441b7c81d28e9d
Keino.Eliud <elkeino@gmail.com>
2022-08-25 15:29:04 +0300

    iserting at a given position

--- Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_freeCode/Data_structures/LinkedList_Rev_2/instertAt_nth_position.c
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_freeCode/Data_structures/LinkedList_Rev_2/instertAt_nth_position.c
@@ -13,21 +13,46 @@ Node_t *head;
 
 // prototypes
 void insert(int data, int pos);
+//Node_t *insert(Node_t *head, int data, int position);
 void printList();
 
 int main()
 {
+    //Node_t *head;
     head = NULL; // initialy the list is empty
     // you can promt user for data input
 
-    insert(2, 1); // List: 2
-    insert(3, 2); // List: 2,3
-    insert(4, 1); // List: 4,2,3
-    insert(5, 2); // List: 4,5,2,3
+    insert( 2, 1); // List: 2
+    insert( 3, 2); // List: 2,3
+     insert( 4, 1); // List: 4,2,3
+   insert( 5, 2); // List: 4,5,2,3
     printList();
 
     return 0;
-}
+   }
+// // insert
+// Node_t *insert(Node_t *head, int data, int position)
+// {
+//     Node_t *temp1 = (Node_t *)malloc(sizeof(Node_t));
+//     temp1->data = data; // set data field
+//     temp1->next = NULL; // set link
+//     if (position == 1) // incase of first node
+//     {
+//         temp1 = temp1->next;
+//         head = temp1;
+//         return;
+//     }
+
+//     // other cases
+//     Node_t* temp2 = head;
+//     for (int i = 0; i < position - 2; i++)
+//     {
+//         temp2 = temp2->next;
+//     }
+//     temp2->next = temp1;
+//     return temp1;    
+
+// }
 // insert
 void insert(int data, int position)
 {
@@ -43,13 +68,13 @@ void insert(int data, int position)
         return;
     }
     // List is not empty or 1st
-    Node_t *temp2 = head;
-    for (int i = 0; i < position - 2; i++)
+    Node_t *temp2 = head; // store address of head
+    for (int i = 0; i < position - 2; i++) // travers the list
     {
-        temp2 = temp2->next;
+        temp2 = temp2->next; // keep going to the next node
     }
-    temp1->next = temp2->next;
-    temp2->next = temp1;
+    temp1->next = temp2->next; // update link parts of the newly created node
+    temp2->next = temp1;// update link parts of the previous node
 }
 // print
 void printList()
---
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_naso_AC/inserting_at_position/insert_at_position.c
@@ -0,0 +1,10 @@
+ // inserting at a given position
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ int main()
+ {
+    
+    return 0;
+ }
+ 
