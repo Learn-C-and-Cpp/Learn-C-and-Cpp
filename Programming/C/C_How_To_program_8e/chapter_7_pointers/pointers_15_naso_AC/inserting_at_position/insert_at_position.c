2d2190ab9eee8810003cacdc1326deb830f31bd0
Keino Eliud <elkeino@gmail.com>
2022-08-27 23:10:59 +0300

    insert at position

--- Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_naso_AC/inserting_at_position/insert_at_position.c
+++ Programming/C/C_How_To_program_8e/chapter_7_pointers/pointers_15_naso_AC/inserting_at_position/insert_at_position.c
@@ -13,7 +13,10 @@ Node_t *head;
 
 // prototypes
 void add_at_end(Node_t *head, int data);
+void add_at_position(Node_t* head, int value, int position);
 void printList(Node_t *head);
+
+int data2 = 4;
 
 int main()
 {
@@ -22,9 +25,20 @@ int main()
    head->next = NULL;
 
    // printList(head);
-   add_at_end(head, 67);
    add_at_end(head, 98);
+   add_at_end(head, 3);
+
+   // add at position
+   int value = 67, position = 3;
+   add_at_position(head, value, position);
    
+   for (int i = 0; i < 4; i++)
+   {
+      add_at_position(head, data2, 4);
+      data2 *= 2;
+  }
+  
+   //add_at_position(head, 4, 4);
 
    printList(head);
 
@@ -32,8 +46,9 @@ int main()
 }
 void add_at_end(Node_t *head, int data)
 {
-   Node_t *temp, *ptr;
-   ptr = head; // ptr to traverse
+   // travese the list and add node at end
+   Node_t *temp;  //store new node
+   Node_t* ptr = head; // ptr points to head for traversing the list
 
    // create a new node
    temp = (Node_t *)malloc(sizeof(Node_t));
@@ -46,6 +61,22 @@ void add_at_end(Node_t *head, int data)
    }
    // update ptr->link with new node
    ptr->next = temp;
+}
+// add node at position
+void add_at_position(Node_t* head, int value, int position)
+{
+   Node_t *temp = head; // traverse list
+   Node_t *temp2 = (Node_t*)malloc(sizeof(Node_t));
+   temp2->data = value;
+   temp2->next = NULL;
+   
+   while (position != 2) // move pointer on position foward to insert between 2 and 3
+   {
+      temp = temp->next;
+      position--;
+   }
+   temp2->next = temp->next;
+   temp->next = temp2;
 }
 // print List
 void printList(Node_t *head)
