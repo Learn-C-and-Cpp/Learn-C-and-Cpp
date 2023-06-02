64a1df2a8d54841a4153432bf8e2831f4ff34d59
kensus <elkeino@gmail.com>
2023-06-02 21:40:20 +0300

    started section 12

---
+++ C++/Udemy/Section_12/SimplePointers/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    int num{};
+    //double a{};
+    std::cout << "Value of num is: " << num << std::endl;
+    std::cout << "Size of num is: " << sizeof(num)  << std::endl;
+    std::cout << "Address of num is: " << &num << std::endl;
+
+
+    return 0;
+}
