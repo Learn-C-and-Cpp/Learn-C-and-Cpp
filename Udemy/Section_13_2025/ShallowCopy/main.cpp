dc6df1b39bf23bec0859951080367fd26412781e
kensus <ekeino@gmail.com>
2025-04-10 23:26:37 +0300

    Added shallow and deep copy

---
+++ Udemy/Section_13_2025/ShallowCopy/main.cpp
@@ -0,0 +1,48 @@
+// Shallow copy
+#include <iostream>
+
+class Shallow
+{
+private:
+    // attributes
+    int *data;
+
+public:
+    // methods
+    void set_data_value(int d) { *data = d; }
+    int get_data_value() { return *data; }
+    // Constructor
+    Shallow(int d);
+    Shallow(const Shallow &source);
+    ~Shallow();
+};
+
+Shallow::Shallow(int d)
+{
+    data = new int; // allocate space
+    *data = d;      // store d in the space
+}
+Shallow::Shallow(const Shallow &source)
+    : data(source.data)
+{
+    std::cout << "Copy constructor - shallow copy" << std::endl;
+}
+Shallow::~Shallow(){
+    delete data;
+    std::cout << "Destructor freeing data" << std::endl;
+}
+void display_shallow(Shallow s){
+    std::cout << s.get_data_value() << std::endl;
+}
+int main()
+{
+    Shallow obj1{100};
+    display_shallow(obj1);
+
+    Shallow obj2{obj1};
+    obj2.set_data_value(1000);
+
+    // std::cout << frank.get_name() << std::endl;
+
+    return 0;
+}
